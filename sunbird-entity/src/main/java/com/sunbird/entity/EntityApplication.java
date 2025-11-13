dc96f98a57061fbb3253486c56271912a201bf89
snehal-gothe <snehal.gothe@tarento.com>
2025-11-13 11:43:43 +0530

    allow origins fix

--- sunbird-entity/src/main/java/com/sunbird/entity/EntityApplication.java
+++ sunbird-entity/src/main/java/com/sunbird/entity/EntityApplication.java
@@ -44,8 +44,11 @@ public class EntityApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods(GET, POST, PUT, DELETE, OPTIONS).allowedOriginPatterns("*")
-						.allowedHeaders("*").allowCredentials(true);
+				registry.addMapping("/**")
+						.allowedOriginPatterns("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
 			}
 
 			@Autowired
