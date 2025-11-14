30ffa2bf24ab51bd6e4f8003e79e546c35b87857
snehal-gothe <snehal.gothe@tarento.com>
2025-11-14 16:03:08 +0530

    allow origins fix

--- sunbird-entity/src/main/java/com/sunbird/entity/EntityApplication.java
+++ sunbird-entity/src/main/java/com/sunbird/entity/EntityApplication.java
@@ -48,7 +48,7 @@ public class EntityApplication {
 						.allowedOriginPatterns("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
-						.allowCredentials(false);
+						.allowCredentials(true);
 			}
 
 			@Autowired
@@ -65,7 +65,7 @@ public class EntityApplication {
 	public FilterRegistrationBean corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(false);
+		config.setAllowCredentials(true);
 		config.addAllowedOriginPattern("*");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
